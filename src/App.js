c2bb7167a94d619f65f62fbb88ad488ba39f6ae1
bhnord <b.nordemann@aol.com>
2023-10-27 11:46:00 -0400

    added basic navbar

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import "./App.css";
 import { Route, Routes } from "react-router-dom";
 import SiteManagerPage from "./pages/SiteManagerPage";
 import HomePage from "./pages/HomePage";
+import Navbar from "./components/navbar/Navbar";
 
 function App() {
   return (
     <div className="App">
+      <Navbar></Navbar>
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/sitemanager" element={<SiteManagerPage />} />
---
+++ src/components/navbar/Navbar.js
@@ -0,0 +1,7 @@
+import "./Navbar.css"
+export default function Navbar() {
+  return <div class="wrapper">
+    <a href="/">Home</a>
+    <a href="/sitemanager">Site Manager Home</a>
+  </div>;
+}
