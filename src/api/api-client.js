bbfbcf435bdf82c8a37f49822a700fe475df8888
bhnord <b.nordemann@aol.com>
2023-11-04 23:36:55 -0400

    api fix

--- src/api/api-client.js
+++ src/api/api-client.js
@@ -1,11 +1,16 @@
 const aws_path = "https://pttmkhd20b.execute-api.us-east-2.amazonaws.com/Prod/";
 
-export function postGenerateStoreInventory(storeName) {
+//TODO: remove this as hard-coded values
+let username = 'sam'
+let password = 'samantha'
+let storeName = 'Sam Store'
+
+export function generateStoreInventory(storeName) {
   const endpoint = "generate-store-inventory";
   const requestOptions = {
     method: "POST",
     headers: { "Content-Type": "application/json" },
-    body: JSON.stringify({ storeName: storeName }),
+    body: JSON.stringify({ username: username, password: password, storeName: storeName }),
   };
 
   return fetch(aws_path + endpoint, requestOptions).then((response) =>
@@ -13,7 +18,7 @@ export function postGenerateStoreInventory(storeName) {
   );
 }
 
-export function postRemoveStore(storeName) {
+export function removeStore(storeName) {
   const endpoint = "remove-store";
   const requestOptions = {
     method: "POST",
