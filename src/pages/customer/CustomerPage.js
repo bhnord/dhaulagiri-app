9c400601dcce7b29750990d648db3d4ceca83948
abigailalbuquerque <122641078+abigailalbuquerque@users.noreply.github.com>
2023-12-14 14:15:30 -0500

    Filtering is working

--- src/pages/customer/CustomerPage.js
+++ src/pages/customer/CustomerPage.js
@@ -8,7 +8,6 @@ export default function CustomerPage() {
   const [computers, setComputers] = useState([]);
   const [stores, setStores] = useState([]);
   const [refresh, setRefresh] = useState(0);
-  const [check, setCheck] = useState([]);
   const [selectedComputers, setSelectedComputers] = useState([]);
 
   const handleRadioClick = (computerID, checked) => {
@@ -26,98 +25,19 @@ export default function CustomerPage() {
   };
 
   useEffect(() => {
-    const loadComputers = (s) => { 
-      // console.log(s);
-      var totalChecked = 0;
-      var checkAll = false;
-      for(var i = 0; i < s.length; i++){
-
-        // console.log(s[i].storeName);
-        // console.log(document.getElementById(s[[i].storeName]));
-
-        if(check[i]){
-          totalChecked += 1;
-        }
-      }
-      if(totalChecked === 0){
-        totalChecked = s.length;
-        checkAll = true;
-      }
-  
-      var num_processed = 0;  
-      for(var i = 0; i < s.length; i++){
-        var computers = [];
-        const getStoreComputers = async () => {
-
-          if(checkAll || check[i]){
-            const resp = await api.generateStoreInventory(s[i].storeName);
-            const new_computers = resp.computers;
-  
-            if (resp.statusCode !== 200) {
-                alert("error getting computers");
-            }
-            else{
-              computers.push(new_computers);
-              num_processed += 1;
-              if(num_processed === totalChecked){
-                computers = computers.flat(1);
-                setComputers(computers);
-              }
-            }
-          }
-        }
-        
-        getStoreComputers();
-      }
-    }
-
     const getStores = async () => {
-      const resp = await api.listStores();
-      const stores = resp.stores;
+        const resp = await api.listStores();
+        const stores = resp.stores;
 
-      if (resp.statusCode !== 200) {
-          alert("Error fetching store's list.")
+        if (resp.statusCode !== 200) {
+            alert("Error fetching store's list.")
 
-      } else {
-          setStores(stores);
-          loadComputers(stores);
-      }
+        } else {
+            setStores(stores);
+        }
     };
-
     getStores();
-  }, [refresh, check, selectedComputers]);
-
-  useEffect(() => {
-    const getStores = async () => {
-      const resp = await api.listStores();
-      const stores = resp.stores;
-
-      if (resp.statusCode !== 200) {
-          alert("invalid login")
-
-      } else {
-          setStores(stores);
-          setCheck(new Array(stores.length).fill(false))
-      }
-    };
-
-    getStores();
-  }, [])
-
-  const clickStore = (store) => {
-    var c = check;
-    // console.log(c);
-    
-    for(var i = 0; i < stores.length; i++){
-      if(stores[i].storeName === store){
-        c[i] = document.getElementById(store).checked;
-        // console.log(c[i])
-      }
-    }
-    setCheck(c);
-    setRefresh(refresh+1);
-  }
-
+  }, [computers, selectedComputers, refresh]);
 
   const compare = async () => {
     if (selectedComputers.length === 2) {
@@ -132,8 +52,6 @@ export default function CustomerPage() {
         container.classList.add(styles.compareSpecs, styles.container);
   
         const resp = await api.generateStoreInventory(computer.storeName, 100, 19.5);
-        console.log("Shipping")
-        console.log(resp)
         const shippingPrice = resp.computers.find((comp) => comp.computerID === computer.computerID)?.shippingPrice;
   
         container.innerHTML = `
@@ -164,54 +82,169 @@ export default function CustomerPage() {
     } else {
       alert("Please select exactly two computers for comparison.");
     }
-  };  
+  };
 
 
 
-  const filter = () => { 
+  const filter = async () => {
+    console.log("Filter button clicked");
     const elem = document.getElementById("StoreSelect");
-    const v = elem.value;
+    const storeName = elem.value;
+  
+    try {
+      if (storeName !== "any") {
+        const features = collectSelectedFeatures();
 
-    if(v !== "any"){
-      const getStoreComputers = async () => {
-          const resp = await api.generateStoreInventory(v);
-          const computers = resp.computers;
-
-          if (resp.statusCode !== 200) {
-              alert("Error generating store inventory.")
-
+        if (features.length == 0){
+          const resp = await api.filterComputer(storeName, null);
+          if (resp && resp.statusCode && resp.statusCode === 200) {
+            setComputers(resp.computers);
           } else {
-              setComputers(computers);
-          }
-      };
-      getStoreComputers();
-    }
-
-    else {
-      var num_processed = 1;
-      for(var i = 1; i < elem.options.length; i++){
-        var computers = [];
-        const getStoreComputers = async () => {
-          const resp = await api.generateStoreInventory(elem.options[i].value);
-          const new_computers = resp.computers;
-
-          if (resp.statusCode !== 200) {
-              alert("Error generating store inventory.");
-          }
-          else{
-            computers.push(new_computers);
-            num_processed += 1;
-            if(num_processed === elem.options.length){
-              computers = computers.flat(1);
-              setComputers(computers);
-            }
+            alert("Error filtering computers.");
           }
         }
-        getStoreComputers();
+
+        else{
+          const resp = await api.filterComputer(storeName, features);
+          console.log(features)
+          console.log(resp)
+          if (resp && resp.statusCode && resp.statusCode === 200) {
+            setComputers(resp.computers);
+          } else {
+            alert("Error filtering computers.");
+          }
+        }
+
       }
+      else if (storeName == "any") {
+        const features = collectSelectedFeatures();
+        
+        if (features.length == 0){
+          const resp = await api.filterComputer(null, null);
+          if (resp && resp.statusCode && resp.statusCode === 200) {
+            setComputers(resp.computers);
+          } else {
+            alert("Error filtering computers.");
+          }
+        }
+
+        else{
+          const resp = await api.filterComputer(null, features);
+          if (resp && resp.statusCode && resp.statusCode === 200) {
+            setComputers(resp.computers);
+          } else {
+            alert("Error filtering computers.");
+          }
+        }
+      }
+    } 
+    catch (error) {
+      console.error("An error occurred:", error);
+      alert("An error occurred while filtering computers.");
     }
-    setRefresh(refresh+1);
+    setRefresh(refresh + 1);
+  };
+  
+
+const collectSelectedFeatures = () => {
+  const features = [];
+
+  //ram
+  if (document.getElementById("4GB").checked) {
+      features.push("4GB or Less");
   }
+  if (document.getElementById("8GB").checked) {
+    features.push("8GB");
+  }
+  if (document.getElementById("16GB").checked) {
+    features.push("16GB");
+  }
+  if (document.getElementById("32GB").checked) {
+    features.push("32GB or more");
+  }
+
+  //storage stuff
+  if (document.getElementById("256GB").checked) {
+    features.push("256GB or less");
+  }
+  if (document.getElementById("512GB").checked) {
+    features.push("512GB");
+  }
+  if (document.getElementById("1TB").checked) {
+    features.push("1TB");
+  }
+  if (document.getElementById("2TB").checked) {
+    features.push("2TB or more");
+  }
+
+  //processor stuff
+  if (document.getElementById("AllIntelProcessor").checked) {
+    features.push("All Intel Processor");
+  }
+  if (document.getElementById("AllAMDProcessor").checked) {
+    features.push("All AMD Processor");
+  }
+
+  //graphics stuff
+  if (document.getElementById("AllIntelGraphics").checked) {
+    features.push("All Intel Graphics");
+  }
+  if (document.getElementById("AllAMDGraphics").checked) {
+    features.push("All AMD Graphics");
+  }
+  if (document.getElementById("AllNVIDIAGraphics").checked) {
+    features.push("All NVIDIA Graphics");
+  }
+
+  //price stuff
+  if (document.getElementById("500").checked) {
+    features.push("$500 or less");
+  }
+  if (document.getElementById("1000").checked) {
+    features.push("$501 to 1000");
+  }
+  if (document.getElementById("1500").checked) {
+    features.push("$1001 to 1500");
+  }
+  if (document.getElementById("2000").checked) {
+    features.push("$1501 to 2000");
+  }
+  if (document.getElementById("2500").checked) {
+    features.push("$2001 or more");
+  }
+  return features;
+};
+
+useEffect(() => {
+  const getStoresAndComputers = async () => {
+    try {
+      const storesResp = await api.listStores();
+      const stores = storesResp.stores;
+
+      if (storesResp.statusCode !== 200) {
+        alert("Error fetching store's list.");
+        return;
+      }
+
+      setStores(stores);
+
+      const initialComputersResp = await api.filterComputer(null, null);
+
+      if (initialComputersResp.statusCode === 200) {
+        setComputers(initialComputersResp.computers);
+      } else {
+        alert("Error fetching initial list of computers.");
+      }
+    } 
+    catch (error) {
+      console.error("An error occurred:", error);
+      alert("An error occurred while fetching data.");
+    }
+  };
+
+  getStoresAndComputers();
+}, []);
+
 
   return (
     <div className={styles.wrapper}>
@@ -221,67 +254,72 @@ export default function CustomerPage() {
 
           <label>Choose a store: </label>
           <form>
-            <div className={styles.checkbox_div}>
+            <select name="Store" id="StoreSelect">
+              <option value="any" key="any">Any from list</option>
               {stores.map((store) => <Store key={store.store_name}
                     refresh={refresh}
                     setRefresh={setRefresh}
-                    checkStore={clickStore}
                     store_data={store}/>)}
+            </select>
+          </form>
+          <br></br>
+
+          <form>
+          <label>RAM: </label>
+            <div className={styles.checkbox_div}>
+              <label><input className={styles.checkbox} type="checkbox" id="4GB" name="RAM" value="4GB or less" />4GB or less</label>
+              <label><input className={styles.checkbox} type="checkbox" id="8GB" name="RAM" value="8GB"></input>8GB</label>
+              <label><input className={styles.checkbox} type="checkbox" id="16GB" name="RAM" value="16GB"></input>16GB</label>
+              <label><input className={styles.checkbox} type="checkbox" id="32GB" name="RAM" value="32GB or more"></input>32GB or more</label>
             </div>
           </form>
           <br></br>
 
           <form>
+          <label>Storage: </label>
             <div className={styles.checkbox_div}>
-              <label><input className={styles.checkbox}  type="checkbox" id="4GB" name="RAM" value="4GB or less" />4GB or less</label>
-              <label><input className={styles.checkbox}  type="checkbox" id="8GB" name="RAM" value="8GB"></input>8GB</label>
-              <label><input className={styles.checkbox}  type="checkbox" id="16GB" name="RAM" value="16GB"></input>16GB</label>
-              <label><input className={styles.checkbox}  type="checkbox" id="32GB" name="RAM" value="32GB or more"></input>32GB or more</label>
-            </div>
-          </form>
-          <br></br>
-
-          <form>
-            <div className={styles.checkbox_div}>
-              <label><input className={styles.checkbox}  type="checkbox" id="256GB" name="Storage" value="256GB or less"></input>256GB or less</label>
-              <label><input className={styles.checkbox}  type="checkbox" id="512GB" name="Storage" value="512GB"></input>512GB</label>
-              <label><input className={styles.checkbox}  type="checkbox" id="1TB" name="Storage" value="1TB"></input>1TB</label>
-              <label><input className={styles.checkbox}  type="checkbox" id="2TB" name="Storage" value="2TB or more"></input>2TB or more</label>
-            </div>
-          </form>
-          <br></br>
-
-
-          <form>
-            <div className={styles.checkbox_div}>
-              <label><input className={styles.checkbox}  type="checkbox" id="AllIntel" name="ProcessorType" value="All Intel"></input>All Intel</label>
-              <label><input className={styles.checkbox}  type="checkbox" id="AllAMD" name="ProcessorType" value="All AMD"></input>All AMD</label>
-            </div>
-          </form>
-          <br></br>
-
-          <form>
-            <div className={styles.checkbox_div}>
-              <label><input className={styles.checkbox}  type="checkbox" id="AllIntel" name="Graphics" value="All Intel"></input>All Intel</label>
-              <label><input className={styles.checkbox}  type="checkbox" id="AllAMD" name="Graphics" value="All AMD"></input>All AMD</label>
-              <label><input className={styles.checkbox}  type="checkbox" id="AllNVIDIA" name="Graphics" value="All NVIDIA"></input>All NVIDIA</label>
+              <label><input className={styles.checkbox} type="checkbox" id="256GB" name="Storage" value="256GB or less"></input>256GB or less</label>
+              <label><input className={styles.checkbox} type="checkbox" id="512GB" name="Storage" value="512GB"></input>512GB</label>
+              <label><input className={styles.checkbox} type="checkbox" id="1TB" name="Storage" value="1TB"></input>1TB</label>
+              <label><input className={styles.checkbox} type="checkbox" id="2TB" name="Storage" value="2TB or more"></input>2TB or more</label>
             </div>
           </form>
           <br></br>
 
 
           <form>
+          <label>Processor: </label>
             <div className={styles.checkbox_div}>
-              <label><input className={styles.checkbox}  type="checkbox" id="500" name="Price" value="$500 or less"></input>$500 or less</label>
-              <label><input className={styles.checkbox}  type="checkbox" id="1000" name="Price" value="$501 to $1000"></input>$501 to $1000</label>
-              <label><input className={styles.checkbox}  type="checkbox" id="1500" name="Price" value="$1001 to $1500"></input>$1001 to $1500</label>
-              <label><input className={styles.checkbox}  type="checkbox" id="2000" name="Price" value="$1501 to $2000"></input>$1501 to $2000</label>
-              <label><input className={styles.checkbox}  type="checkbox" id="2500" name="Price" value="$2001 or more"></input>$2001 or more</label>
+              <label><input className={styles.checkbox} type="checkbox" id="AllIntelProcessor" name="ProcessorType" value="All Intel"></input>All Intel</label>
+              <label><input className={styles.checkbox} type="checkbox" id="AllAMDProcessor" name="ProcessorType" value="All AMD"></input>All AMD</label>
             </div>
           </form>
           <br></br>
 
-          {/* <button onClick>Filter Computers</button> */}
+          <form>
+          <label>Graphics: </label>
+            <div className={styles.checkbox_div}>
+              <label><input className={styles.checkbox} type="checkbox" id="AllIntelGraphics" name="Graphics" value="All Intel"></input>All Intel</label>
+              <label><input className={styles.checkbox} type="checkbox" id="AllAMDGraphics" name="Graphics" value="All AMD"></input>All AMD</label>
+              <label><input className={styles.checkbox} type="checkbox" id="AllNVIDIAGraphics" name="Graphics" value="All NVIDIA"></input>All NVIDIA</label>
+            </div>
+          </form>
+          <br></br>
+
+
+          <form>
+          <label>Price: </label>
+            <div className={styles.checkbox_div}>
+              <label><input className={styles.checkbox} type="checkbox" id="500" name="Price" value="$500 or less"></input>$500 or less</label>
+              <label><input className={styles.checkbox} type="checkbox" id="1000" name="Price" value="$501 to $1000"></input>$501 to $1000</label>
+              <label><input className={styles.checkbox} type="checkbox" id="1500" name="Price" value="$1001 to $1500"></input>$1001 to $1500</label>
+              <label><input className={styles.checkbox} type="checkbox" id="2000" name="Price" value="$1501 to $2000"></input>$1501 to $2000</label>
+              <label><input className={styles.checkbox} type="checkbox" id="2500" name="Price" value="$2001 or more"></input>$2001 or more</label>
+            </div>
+          </form>
+          <br></br>
+
+          <button onClick={filter}>Filter Computers</button>
         </div>
         <div id={styles.computer_view}>
         {computers.map((comp) => (
